841db88c2a47d8d3e368bfa7befbb4047516aec4
Dan Mahoney <dan@thevirtualsailloft.com>
2015-07-16 18:25:45 -0400

    new file:   .gitignore 	new file:   Makefile 	new file:   include/example.h 	new file:   include/sensor.h 	new file:   src/main.cpp 	new file:   src/sensor.cpp

---
+++ src/sensor.cpp
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------------------
+//
+// <sensor>.cpp
+// This is the base class for all <sensor>s
+//
+//
+// Author: Dan Mahoney
+// ---------------------------------------------------------------------------
+#include "sensor.h"
+
+Sensor::Sensor() {}
+
+Sensor::~Sensor() {}
+
+void Sensor::getData(){}
+
+void Sensor::getDataFormat() {}
